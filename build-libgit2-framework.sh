c2d44c7ab57074985a342c0a254ceee4c4bc3fd0
amemiya <74266094+amemiya@users.noreply.github.com>
2025-06-16 23:12:44 +0900

    どうなる？

--- build-libgit2-framework.sh
+++ build-libgit2-framework.sh
@@ -135,12 +135,14 @@ function build_mbedtls() {
 	esac
 
 	# See https://wiki.openssl.org/index.php/Compilation_and_Installation
-	cmake -S "$REPO_ROOT/mbedtls-3.6.3.1" -B "$REPO_ROOT/install/$PLATFORM" \
+	cmake -S . -B "$REPO_ROOT/install/$PLATFORM" \
     -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_OSX_SYSROOT=$PLATFORM \
+    -DCMAKE_OSX_SYSROOT=$SYSROOT \
     -DCMAKE_OSX_ARCHITECTURES=$ARCH \
     -DCMAKE_INSTALL_PREFIX=$REPO_ROOT/install/$PLATFORM \
-    -DCMAKE_POSITION_INDEPENDENT_CODE=ON
+    -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
+    -DENABLE_PROGRAMS=OFF \
+    -DENABLE_TESTING=OFF
     cmake --build "$REPO_ROOT/install/$PLATFORM" --config Release
 	export -n CFLAGS
 }
